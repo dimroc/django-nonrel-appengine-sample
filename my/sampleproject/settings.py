a78c80219ee856cd18f680a496c91ed0f6942658
Dimitri Roche <dimroc@gmail.com>
2011-04-26 20:40:15 -0400

    modified settings.py to include media generator settings and piston.

--- my/sampleproject/settings.py
+++ my/sampleproject/settings.py
@@ -5,7 +5,8 @@ import os
 # of "djangoappengine" from this file.
 from djangoappengine.settings_base import *
 
-SECRET_KEY = 'moowashere'
+SECRET_KEY = 'dimitri_was_here'
+DEBUG = True
 
 INSTALLED_APPS = (
 #    'django.contrib.admin',
@@ -14,12 +15,16 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'djangotoolbox',
+    'django-piston',
+    'mediagenerator',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
 )
 
 MIDDLEWARE_CLASSES = (
+    # Media middleware has to come first.
+    'mediagenerator.middleware.MediaMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -34,6 +39,17 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # This test runner captures stdout and associates tracebacks with their
 # corresponding output. Helps a lot with print-debugging.
 TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
+
+# MediaGenerator.Middleware Definitions.
+# NOTE: This was built pre django 1.3 where media == static
+# NOTE: media has since come to mean uploaded content, but the names
+# NOTE: below mean static.
+MEDIA_DEV_MODE = DEBUG
+DEV_MEDIA_URL = '/devstatic/'
+PRODUCTION_MEDIA_URL = '/static/'
+
+GLOBAL_MEDIA_DIRS = (os.path.join(os.path.dirname(__file__), 'static'),)
+# End of Media.Generator.Middleware Definitions.
 
 ADMIN_MEDIA_PREFIX = '/media/admin/'
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
